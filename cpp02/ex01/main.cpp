ea0907f0ab78d0bda22b3193da0bca5986b8009e
tonypony220 <61715244+tonypony220@users.noreply.github.com>
2021-09-06 13:59:01 +0300

    auto_up

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -1,19 +1,38 @@
 # include "Fixed.hpp"
 #define FRAC 8	
 
+void put_bin( int raw , int size ) {
+	int bit; 
+	for (int i = size - 1; 0 <= i ; i--) {
+		bit = (int)((bool)(raw & 1 << i) && (bool)1);
+		std::cout << bit;
+	}
+	std::cout << std::endl;
+}
+
 int main() {
 	//Fixed a;
 	float a = 0.15625;
     union ieee754_float *dl;
+	int d;
     dl = (union ieee754_float*)&a;
 	
+	d = *((int*)&dl->f);
+	put_bin( d, 32 );
 	std::cout << dl->ieee.exponent - 127 << "\n";
+	int exp = dl->ieee.exponent - 127;
 	dl->ieee.exponent = 0;
 	dl->ieee.negative = 0;
-	int d = *((int*)&dl->f);
-	d = d >> 1;
-	d |= 1 << 31;	
-	d = d >> (31 - FRAC + dl->ieee.exponent - 127);
+	d = *((int*)&dl->f);
+	put_bin( d, 32 );
+	//d = d >> 1;
+	d |= 1 << 23;	
+	put_bin( d, 32 );	
+	int shift = (23 - FRAC - exp);
+	std::cout << "shitf:" << shift  << std::endl; 
+	d = d >> shift;
+//	put_bin( 32, 32 );
+	put_bin( d, 32 );
 			
 	std::cout << (1 / a - roundf(a)) << "\n";
 }
