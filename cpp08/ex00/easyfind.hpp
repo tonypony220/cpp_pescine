28d8a7c9d76261e7b424bf8b6adb041175aaa3fc
Marq Ehtel <mehtel@mi-m3.msk.21-school.ru>
2021-10-19 23:58:34 +0300

    auto_up

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -11,6 +11,8 @@
 //As such, the compiler can't inspect list<tNode<T>*> (it doesn't have its definition at this point) and so it doesn't know whether list<tNode<T>*>::iterator is either a static field or a type.
 //In such a situation, the compiler assumes that it is a field, so in your case it yields a syntax error. To solve the issue, just tell the compiler that it is a type by putting a typename ahead of the declaration:
 
+// https://ru.stackoverflow.com/questions/239051/Зачем-надо-писать-typename-для-уточнения-типа
+
 template <class T>
 typename T::iterator easyfind (T & iterable, int num) {
 	return find( iterable.begin(), iterable.end(), num );
--- cpp08/ex02/mutantstack.hpp
+++ cpp08/ex02/mutantstack.hpp
@@ -1,27 +1,68 @@
 #include <iostream>
 #include <stack>
+#include <algorithm>
 #pragma once
 
 //https://codereview.stackexchange.com/questions/189380/example-of-adding-stl-iterator-support-to-custom-collection-class
 //https://internalpointers.com/post/writing-custom-iterators-modern-cpp
+
 template <class T>
 class MutantStack : public std::stack<T> {
-
-//	private:
-//		unsigned int	 size;
-//		std::vector<int> arr;
-//	public:
-//		Span( unsigned int );
-//		Span( const Span & copy );
-//		~Span( void );
-//		Span & operator=( const Span & other );
-//
-//		unsigned int	 getSize( void ) const ;
-//		std::vector<int> getArr( void )  const ;
-//		int				 getSpan( bool ) ;
-//		int  			 shortestSpan( void );
-//		int  			 longestSpan( void ) ;
-//		void 			 addNumber( int );
-//		void 			 addNumbers( std::vector<int>::iterator & ,
-//									 std::vector<int>::iterator & ) ;
+	public:
+	//	typedef typename std::deque<T>::iterator iterator;
+		typedef typename std::stack<T>::container_type::iterator iterator;
+	//	typedef typename MutantStack::container_type::iterator iterator;
+		iterator  begin( void ) { return MutantStack.container_type.begin(); }
+		iterator  end( void )   { return MutantStack.container_type.end(); }
 };
+
+
+//template <class T, class Container = std::deque<T> >
+//class MutantStack : public std::stack< T, Container > {
+//		Container arr;
+//
+//	public:
+//		MutantStack( void );
+//		~MutantStack( void );
+//		MutantStack( const MutantStack & copy );
+//		MutantStack & operator=( const MutantStack & other );
+//		void 						  push( T );
+//		T &  						  top( void );
+//		void  						  pop( void );
+//		typename Container::size_type size( void );
+//		bool 						  empty( void );
+//
+//		typedef typename Container::iterator iterator;
+//		typename Container::iterator  begin() { return arr.begin(); }
+//		typename Container::iterator  end()   { return arr.end(); }
+//};
+//
+//template <class T, class Container>
+//MutantStack<T, Container>::MutantStack( void ) {}
+//
+//template <class T, class Container>
+//MutantStack<T, Container>::~MutantStack( void ) {}
+//
+//template <class T, class Container>
+//MutantStack<T, Container>::MutantStack( const MutantStack<T, Container> & copy) { *this = copy; }
+//
+//template <class T, class Container>
+//void MutantStack<T, Container>::push( T t ) { arr.push_back(t); }
+//
+//template <class T, class Container>
+//void MutantStack<T, Container>::pop( void ) { arr.pop_back(); }
+//
+//template <class T, class Container>
+//T & MutantStack<T, Container>::top( void ) { return arr.back(); }
+//
+//template <class T, class Container>
+//typename Container::size_type MutantStack<T, Container>::size( void ) { return arr.size(); }
+//
+//template <class T, class Container>
+//bool MutantStack<T, Container>::empty( void ) { return arr.empty(); }
+//
+//template <class T, class Container>
+//MutantStack<T, Container> & MutantStack<T, Container>::operator=( const MutantStack<T, Container> & other ){
+//	arr = other.arr;
+//	return *this;
+//}
